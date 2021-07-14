5c0a41d6e968e15bda3b49b8a2a9514a857c39be
end0tknr <end0tknr@gmail.com>
2021-07-14 15:38:40 +0900

    Add files via upload

---
+++ js/my_tag_manager.js
@@ -0,0 +1,21 @@
+// Google Tag Manager
+(function(w,d,s,l,i){
+    w[l]=w[l]||[];w[l].push({ 'gtm.start':new Date().getTime(), event:'gtm.js' });
+    var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),
+        dl=l!='dataLayer'?'&l='+l:'';
+    j.async=true;
+    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NFDXJK8');
+
+// Google Tag Manager - no script
+
+var noscript_elm = $("<noscript></noscript>");
+var iframe_elm =
+    $("<iframe></iframe>",{
+        iframe_elm.src   :"https://www.googletagmanager.com/ns.html?id=GTM-NFDXJK8",
+        iframe_elm.height:"0",
+        iframe_elm.width :"0",
+        iframe_elm.style :"display:none;visibility:hidden" });
+$("body").prepend( $(noscript_elm).append(iframe_elm) );
+
