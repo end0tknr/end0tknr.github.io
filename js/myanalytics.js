32bdf3c65d4b30f391b8313ba8e736a1f2569500
end0tknr <end0tknr@gmail.com>
2021-07-14 14:58:01 +0900

    Add files via upload

---
+++ js/myanalytics.js
@@ -0,0 +1,8 @@
+var script_elm = document.createElement('script');
+script_elm.src = "https://www.googletagmanager.com/gtag/js?id=G-Q45JN65SKT";
+document.head.appendChild(script_elm);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-Q45JN65SKT');
