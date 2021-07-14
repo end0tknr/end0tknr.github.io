b91f4558fb2f6ee31d71f3e5407991e2f4dc4d08
end0tknr <end0tknr@gmail.com>
2021-07-14 15:38:53 +0900

    Delete myanalytics.js

--- js/myanalytics.js
+++
@@ -1,8 +0,0 @@
-var script_elm = document.createElement('script');
-script_elm.src = "https://www.googletagmanager.com/gtag/js?id=G-Q45JN65SKT";
-document.head.appendChild(script_elm);
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-Q45JN65SKT');
